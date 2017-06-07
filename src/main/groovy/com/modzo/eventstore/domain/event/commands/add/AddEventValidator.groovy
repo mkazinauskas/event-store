9ff99e3ccf959176a79913816b468da61388a6e4
modestas <modestukasai@gmail.com>
2017-06-07 23:03:00 +0300

    Validation update

--- src/main/groovy/com/modzo/eventstore/domain/event/commands/add/AddEventValidator.groovy
+++ src/main/groovy/com/modzo/eventstore/domain/event/commands/add/AddEventValidator.groovy
@@ -1,22 +1,34 @@
 package com.modzo.eventstore.domain.event.commands.add
 
 import com.modzo.eventstore.domain.DomainException
-import org.apache.commons.lang.StringUtils
+import com.modzo.eventstore.domain.event.Events
 import org.springframework.stereotype.Component
+
+import static org.apache.commons.lang.StringUtils.isBlank
 
 @Component
 class AddEventValidator {
 
+    private final Events events
+
+    AddEventValidator(Events events) {
+        this.events = events
+    }
+
     void validate(AddEvent addEvent) {
-        if (StringUtils.isBlank(addEvent.uniqueId)) {
+        if (isBlank(addEvent.uniqueId)) {
             throw new DomainException('EVENT_UNIQUE_ID', 'Event unique id cannot be blank')
         }
 
-        if (StringUtils.isBlank(addEvent.topic)) {
+        if (events.findByUniqueId(addEvent.uniqueId).present) {
+            throw new DomainException('EVENT_UNIQUE_ID', 'Event unique id is already saved')
+        }
+
+        if (isBlank(addEvent.topic)) {
             throw new DomainException('EVENT_TOPIC', 'Topic cannot be blank')
         }
 
-        if (StringUtils.isBlank(addEvent.value)) {
+        if (isBlank(addEvent.value)) {
             throw new DomainException('EVENT_VALUE', 'Value cannot be blank')
         }
     }
