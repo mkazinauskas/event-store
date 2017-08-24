350b8402beffe6b09b81b76007620d00c8d601ef
modestas <modestukasai@gmail.com>
2017-08-24 23:53:46 +0300

    Id generation update

--- src/main/groovy/com/modzo/eventstore/domain/event/Event.groovy
+++ src/main/groovy/com/modzo/eventstore/domain/event/Event.groovy
@@ -10,7 +10,7 @@ import javax.persistence.*
 @Table(name = 'events')
 class Event {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = 'id', nullable = false)
     Long id
 
