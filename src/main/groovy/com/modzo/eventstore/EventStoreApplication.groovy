bcf2a7849edf43be0f85853c90cac2b1870d8362
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2017-11-01 13:33:17 +0200

    Eureka client enabled

--- src/main/groovy/com/modzo/eventstore/EventStoreApplication.groovy
+++ src/main/groovy/com/modzo/eventstore/EventStoreApplication.groovy
@@ -2,8 +2,10 @@ package com.modzo.eventstore
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class EventStoreApplication {
 
     static void main(String[] args) {
